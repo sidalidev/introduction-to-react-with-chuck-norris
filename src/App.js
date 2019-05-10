c44aba65e929e59407df8679d32630f7e118a2f9
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-05-10 06:08:40 +0200

    Think that skeleton is ready

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import axios from 'axios';
 import Logo from './components/Logo';
 import QuoteItem from './components/QuoteItem';
 import QuoteNew from './components/QuoteNew';
 
-function App() {
-  return (
-    <div className='App'>
-      <Logo />
-      <div className='QuotesWrapper'>
-        <h3>Quotes</h3>
-        <div className='QuoteList'>
-          <QuoteItem />
-          <QuoteItem />
-          <QuoteItem />
-          <QuoteItem />
-          <QuoteItem />
-          <QuoteItem />
-          <QuoteItem />
+// const API_BASE_URL = 'https://api.chucknorris.io/jokes';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Logo />
+        <div className='QuotesWrapper'>
+          <h3>Quotes</h3>
+          <div className='QuoteList'>
+            <QuoteItem />
+          </div>
+          <QuoteNew />
         </div>
-        <QuoteNew />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/QuoteNew.js
+++ src/components/QuoteNew.js
@@ -1,12 +1,16 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
 
 export default class QuoteNew extends React.Component {
   render() {
     return (
       <div className='QuoteNew'>
-        <input type='text' />
+        <input type='text' placeholder='Type your quote here… ' />
         <span className='Clickable' role='img'>
-          💪
+          🆕
+        </span>
+        <span className='Clickable' role='img'>
+          🤘
         </span>
       </div>
     );
